b211a5ee1a58a8421e1ffd23fe1200d656c8b1ce
Michael Cockcroft <cocymsc@hotmail.com>
2019-07-26 22:11:07 +0100

    updated queries for api changes

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,156 +1,228 @@
-import axios from 'axios';
+import axios from "axios";
 
 const root = {
-	player: async ({ id }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const player = data.filter(el => el.id == id)[0];
+  player: async ({ id }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const player = elements.filter(el => el.id == id)[0];
 
-			return player;
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+      return player;
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-	playersByTeam: async ({ team }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const players = data.filter(player => player.team === team);
-			return { players };
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+  playersByTeam: async ({ team }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const players = elements.filter(player => player.team === team);
+      return { players };
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-	playerWithHighestProp: async ({ prop }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const player = data.reduce((a, b) => {
-				if (typeof a[prop] !== Number) {
-					return parseFloat(a[prop]) > parseFloat(b[prop]) ? a : b;
-				}
-				return a[prop] > b[prop] ? a : b;
-			});
+  playerWithHighestProp: async ({ prop }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const player = elements.reduce((a, b) => {
+        if (typeof a[prop] !== Number) {
+          return parseFloat(a[prop]) > parseFloat(b[prop]) ? a : b;
+        }
+        return a[prop] > b[prop] ? a : b;
+      });
 
-			return { player };
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+      return { player };
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-	playerWithLowestProp: async ({ prop }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const player = data.reduce((a, b) => a[prop] < b[prop] ? a : b);
+  playerWithLowestProp: async ({ prop }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const player = elements.reduce((a, b) => (a[prop] < b[prop] ? a : b));
 
-			return { player };
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+      return { player };
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-	playersByProp: async ({ prop, amount, reverseOrder = false }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const players = 
-				data.sort((a, b) => reverseOrder ? a[prop] - b[prop] : b[prop] - a[prop]).slice(0, amount);
-			return { players };
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+  playersByProp: async ({ prop, amount, reverseOrder = false }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const players = elements
+        .sort((a, b) => (reverseOrder ? a[prop] - b[prop] : b[prop] - a[prop]))
+        .slice(0, amount);
+      return { players };
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-	playersSearch: async ({ term, amount = 8 }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const players = 
-				data.filter(({ first_name, second_name }) => {
-					const fullName = `${first_name} ${second_name}`;
-					return fullName.toLowerCase().includes(term.toLowerCase());
-				}).slice(0, amount);
-			return { players };
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+  playersSearch: async ({ term, amount = 8 }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const players = elements
+        .filter(({ first_name, second_name }) => {
+          const fullName = `${first_name} ${second_name}`;
+          return fullName.toLowerCase().includes(term.toLowerCase());
+        })
+        .slice(0, amount);
+      return { players };
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-	playersByPropAndPos: async ({ prop, position, amount, reverseOrder = false }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/elements`);
-			const positionMap = {
-				goalkeeper: 1,
-				defender: 2,
-				midfielder: 3,
-				forward: 4
-			}
-			
-			const players = data
-				.filter(player => player.element_type === positionMap[position])
-				.sort((a, b) => reverseOrder ? a[prop] - b[prop] : b[prop] - a[prop]).slice(0, amount);
+  playersByPropAndPos: async ({
+    prop,
+    position,
+    amount,
+    reverseOrder = false
+  }) => {
+    try {
+      const {
+        data: { elements }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const positionMap = {
+        goalkeeper: 1,
+        defender: 2,
+        midfielder: 3,
+        forward: 4
+      };
 
-			return { players };
-		} catch (err) {
-			console.error('Error getting player data: ', err)
-		}
-	},
+      const players = elements
+        .filter(player => player.element_type === positionMap[position])
+        .sort((a, b) => (reverseOrder ? a[prop] - b[prop] : b[prop] - a[prop]))
+        .slice(0, amount);
 
-	team: async ({ id }) => {
-		try {
-			const { data } = await axios.get(`https://fantasy.premierleague.com/drf/teams`);
-			const team = data.filter(el => el.id == id)[0];
+      return { players };
+    } catch (err) {
+      console.error("Error getting player data: ", err);
+    }
+  },
 
-			return team;
-		} catch (err) {
-			console.error('Error getting team data: ', err)
-		}
-	},
+  team: async ({ id }) => {
+    try {
+      const {
+        data: { teams }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const team = teams.filter(el => el.id == id)[0];
 
-	allTeams: async () => {
-		try {
-			const { data: teams } = await axios.get(`https://fantasy.premierleague.com/drf/teams`);
-			
-			return { teams };
-		} catch (err) {
-			console.error('Error getting team data: ', err)
-		}
-	},
+      return team;
+    } catch (err) {
+      console.error("Error getting team data: ", err);
+    }
+  },
 
-	fixtures: async ({ id = null }) => {
-		try {
-			const { data: gameweekData } = await axios.get(`https://fantasy.premierleague.com/drf/events`);
-			const gameweek = gameweekData.filter(gw => !id ? gw.is_current === true : gw.id == id)[0] ;
+  allTeams: async () => {
+    try {
+      const {
+        data: { teams }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
 
-			const { data: fixtureData } = await axios.get(`https://fantasy.premierleague.com/drf/fixtures`);
-			const fixtures = fixtureData.filter(week => week.deadline_time === gameweek.deadline_time)
-			return { fixtures, id: gameweek.id };
-		} catch (err) {
-			console.error('Error getting fixture data: ', err)
-		}
-	},
+      return { teams };
+    } catch (err) {
+      console.error("Error getting team data: ", err);
+    }
+  },
 
-	getTeamsFixtures: async ({ id, amount }) => {
-		try {
-			const { data: fixtureData } = await axios.get(`https://fantasy.premierleague.com/drf/fixtures`);
-			const fixtures = fixtureData.filter(week => week.started === false && (week.team_a === id || week.team_h === id)).slice(0, amount);
-			return { fixtures, id };
-		} catch (err) {
-			console.error('Error getting teams fixtures: ', err)
-		}
-	},
+  fixtures: async ({ id = null }) => {
+    try {
+      const {
+        data: { events: gameweekData }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
 
-	getAllTeamsFixtures: async () => {
-		try {
-			const { data: fixtureData } = await axios.get(`https://fantasy.premierleague.com/drf/fixtures`);
-			const fixtures = fixtureData.filter(week => week.started === false);
+      let gameweek = gameweekData.find(gw =>
+        !id ? gw.is_current || gw.is_next : gw.id == id
+      );
 
-			const { data: teams } = await axios.get(`https://fantasy.premierleague.com/drf/teams`);
-			const teamSortedFixtures = teams.map(team => fixtures.filter(fixture => fixture.team_h === team.id || fixture.team_a === team.id));
+      const { data: fixtureData } = await axios.get(
+        "https://fantasy.premierleague.com/api/fixtures"
+      );
 
-			return { fixtures: teamSortedFixtures };
-		} catch (err) {
-			console.error('Error getting teams fixtures: ', err)
-		}
-	}
+      const fixtures = fixtureData.filter(week => week.event === gameweek.id);
+
+      return { fixtures, id: gameweek.id };
+    } catch (err) {
+      console.error("Error getting fixture data: ", err);
+    }
+  },
+
+  getTeamsFixtures: async ({ id, amount }) => {
+    try {
+      const { data: fixtureData } = await axios.get(
+        "https://fantasy.premierleague.com/api/fixtures"
+      );
+      const fixtures = fixtureData
+        .filter(
+          week =>
+            week.started === false && (week.team_a === id || week.team_h === id)
+        )
+        .slice(0, amount);
+      return { fixtures, id };
+    } catch (err) {
+      console.error("Error getting teams fixtures: ", err);
+    }
+  },
+
+  getAllTeamsFixtures: async () => {
+    try {
+      const { data: fixtureData } = await axios.get(
+        "https://fantasy.premierleague.com/api/fixtures"
+      );
+      const fixtures = fixtureData.filter(week => week.started === false);
+
+      const {
+        data: { teams }
+      } = await axios.get(
+        "https://fantasy.premierleague.com/api/bootstrap-static"
+      );
+      const teamSortedFixtures = teams.map(team =>
+        fixtures.filter(
+          fixture => fixture.team_h === team.id || fixture.team_a === team.id
+        )
+      );
+
+      return { fixtures: teamSortedFixtures };
+    } catch (err) {
+      console.error("Error getting teams fixtures: ", err);
+    }
+  }
 };
 
 export default root;
